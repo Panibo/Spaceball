f95b4b156e696d3597fc9deb7244460f938ce53f
TommiUp <tuponen@gmail.com>
2021-11-11 12:04:35 +0200

    Level selection, joka lataa oikean scenen

--- Spaceball/Assets/MainMenu.cs
+++ Spaceball/Assets/MainMenu.cs
@@ -5,7 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayLevel3()
     {
         SceneManager.LoadScene(1);
     }
