a4f66891671696762896d4ebf9a63b26153db3c4
TommiUp <tuponen@gmail.com>
2021-11-13 14:21:00 +0200

    Level update

---
+++ Spaceball/Assets/Audio.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSound : MonoBehaviour
+{
+    public AudioClip sound;
+    private Button button { get { return GetComponent<Button>(); } }
+    private static AudioSource soundPlay;
+
+    void Start()
+    {
+
+        button.onClick.AddListener(() => PlaySound());
+
+    }
+    void PlaySound()
+    {
+        soundPlay = Instantiate(gameObject.AddComponent<AudioSource>());
+        soundPlay.clip = sound;
+        soundPlay.playOnAwake = false;
+        soundPlay.PlayOneShot(sound);
+        DestroyObject(soundPlay, 1f);
+    }
+}
